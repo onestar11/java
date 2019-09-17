16269de6a1e467760af1cd8c9e34239727a5106e
onestar11 <54836016+onestar11@users.noreply.github.com>
2019-09-17 15:09:57 +0800

    Create ComputeArea.java

---
+++ ComputeArea.java
@@ -0,0 +1,19 @@
+package Hello;
+
+public class Hello {
+	public static void main(String args[])
+	{
+		double radius;//Declear radius
+		double area;//Declear area
+		
+		//Assign a radius
+		radius=20;//radius is now 20
+		
+		//Compute area
+		area=radius*radius*3.14159;
+		
+		//Display results
+		System.out.println("The area for the circle of radius "+radius+" is " +area);
+		
+	}
+}
