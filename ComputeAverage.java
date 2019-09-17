c62180b2df6ca8a5dabd96a846e9122cf41ddaab
onestar11 <54836016+onestar11@users.noreply.github.com>
2019-09-17 15:24:59 +0800

    Create ComputeAverage.java

---
+++ ComputeAverage.java
@@ -0,0 +1,24 @@
+
+package Hello;
+
+import java.util.Scanner;//Scanner is in the java.util package
+
+public class Hello {
+	public static void main(String args[])
+	{
+		//Creat a Scanner object
+		Scanner input=new Scanner(System.in);
+		
+		//Prompt the user to enter three numbers
+		System.out.print("Enter three numbers: ");
+		double number1=input.nextDouble();
+		double number2=input.nextDouble();
+		double number3=input.nextDouble();
+		
+		//Compute average
+		double average=(number1+number2+number3)/3;
+		
+		//Display results
+		System.out.println("The average of "+number1+" "+number2+" "+number3+" is "+average);
+	}
+}
