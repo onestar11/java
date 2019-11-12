be398e642af8e1d7c652f896c8fb28b40d2ecf48
onestar11 <54836016+onestar11@users.noreply.github.com>
2019-11-12 15:56:12 +0800

    Create RectangleFromSimpleGeometricObjecta .java

---
+++ RectangleFromSimpleGeometricObjecta .java
@@ -0,0 +1,50 @@
+
+public class RectangleFromSimpleGeometricObjecta 
+	extends SimpleGeometricObjecta{
+	private double width;
+	private double height;
+	
+	public RectangleFromSimpleGeometricObjecta(){}
+	
+	public RectangleFromSimpleGeometricObjecta(double width,double height){
+		this.width=width;
+		this.height=height;
+	}
+	
+	public RectangleFromSimpleGeometricObjecta(double width,double height,String color,boolean filled){
+		this.width=width;
+		this.height=height;
+		setColor(color);
+		setFilled(filled);
+	}
+	
+	/**Return width*/
+	public double getWidth(){
+		return width;
+	}
+	
+	/**Set a new width*/
+	public void setWidth(double width){
+		this.width=width;
+	}
+	
+	/**Return height*/
+	public double getHeight(){
+		return height;
+	}
+	
+	/**Set a new height*/
+	public void setHeight(double height){
+		this.height=height;
+	}
+	
+	/**Return area*/
+	public double getArea(){
+		return width*height;
+	}
+	
+	/**Return perimeter*/
+	public double getPerimeter(){
+		return 2*(width+height);
+	}
+}
