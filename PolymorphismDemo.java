c54e690c095692b201471384072f5803dd533276
onestar11 <54836016+onestar11@users.noreply.github.com>
2019-11-18 19:52:26 +0800

    Update PolymorphismDemo.java

--- PolymorphismDemo.java
+++ PolymorphismDemo.java
@@ -7,7 +7,7 @@ public class PolymorphismDemo {
 		displayObject(new RectangleFromSimpleGeometricObjecta(1,1,"black",true));
 	}
 	
-	
+//父类所定义的对象，可以直接表示子类 比如下面的SimpleGeometricObjecta object  用父类定义一个变量 但是后面也可表示一个子类。
 	/**Display geometric object properties*/
 	public static void displayObject(SimpleGeometricObjecta object) {
 		System.out.println("Created on "+object.getDateCreated()+".Colour is "+object.getColor());
